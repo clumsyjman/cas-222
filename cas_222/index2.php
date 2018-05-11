c3a0769dfdc6b1edd2c9f141c81785523228e24d
clumsyjman <38195379+clumsyjman@users.noreply.github.com>
2018-05-11 04:38:41 -0700

    Add files via upload

---
+++ cas_222/index2.php
@@ -0,0 +1,50 @@
+
+
+<html lang="en">
+   
+	
+	
+	
+	<?php
+include_once "includes/head.php";
+?>
+	
+   <body>
+      <header id="header">
+         <div id="name">
+            <h1>h1 </h1>
+         </div>
+      </header>
+      <main>
+         <div class="topnav" id="myTopnav">
+  <a href="#home" class="active">Home</a>
+  <a href="#news">Registration</a>
+  <a href="#contact">FAQ</a>
+  <a href="#about">Contact</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+         <div class="innertube">
+            <div class="hero-section">
+               <div class="hero-section-text">
+                  <h1>Hero image goes here</h1>
+                  <h5>subtitile text goes here</h5>
+               </div>
+            </div>
+            <div class="the-text">
+               <h1>Heading</h1>
+               <p> <br> <br> <br> <br> <br>main <br> <br> <br> <br> <br></p>
+            </div>         
+			 
+			
+			 <?php
+include_once "includes/footer.php";
+?>
+			 
+			 
+			 
+         </div>
+      </main>
+   </body>
+</html>
