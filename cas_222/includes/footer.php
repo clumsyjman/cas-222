fe0af8bfda46de4f5754a24e8491abaced7d4ce0
clumsyjman <38195379+clumsyjman@users.noreply.github.com>
2018-05-11 00:56:15 -0700

    Add files via upload

---
+++ cas_222/includes/footer.php
@@ -0,0 +1,12 @@
+<? php
+echo '<footer class="footer-basic-centered">';
+echo '<p class="footer-company-motto">We offer Events for Every Body</p>';
+echo '<p class="footer-links">';
+echo '<a href="#">Home</a>';
+echo '<a href="#">Registration</a>';
+echo '<a href="#">FAQ</a>';
+echo '<a href="#">Contact</a>';
+echo '</p>';
+echo '<p class="footer-company-name">Ace in the Hole Multisport Events &copy; 2018</p>';
+echo '</footer>';
+?>
