219e52481b34a85fba4cd4d0f831ac32714f42fd
clumsyjman <38195379+clumsyjman@users.noreply.github.com>
2018-05-16 05:37:24 -0700

    Delete head.php

--- cas_222/includes/head.php
+++
@@ -1,8 +0,0 @@
-<head>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-      <title>Ace in the Hole template</title>
-      <link rel="stylesheet" type="text/css" href="styles/styles.css">
-      <script src="script/script.js"></script>
-	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-   </head>
