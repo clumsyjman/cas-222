213b4187f8440cc3f3dae7a95659d40b6484bcd9
clumsyjman <38195379+clumsyjman@users.noreply.github.com>
2018-05-18 06:53:02 -0700

    Delete nav.php

--- cas_222(updated)/includes/nav.php
+++
@@ -1,9 +0,0 @@
-		  <nav class="topnav" id="myTopnav">
-  <a href="#home" class="active">Home</a>
-  <a href="#news">Registration</a>
-  <a href="#contact">FAQ</a>
-  <a href="#about">Contact</a>
-  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-  </a>
-</nav>
